997d750e2973819c6b6f3395544e8aaf994cf7eb
Ciarán Mooney <general.mooney@googlemail.com>
2018-08-03 15:35:42 +0100

    Got trash path incorrect

--- backup-monthly.sh
+++ backup-monthly.sh
@@ -1,3 +1,3 @@
 duplicity remove-all-inc-of-but-n-full 30 --force file:///media/backup
-duplicity --exclude /home/*/.local/Trash /home file:///media/backup
+duplicity --exclude /home/*/.local/share/Trash /home file:///media/backup
 duplicity cleanup --force
