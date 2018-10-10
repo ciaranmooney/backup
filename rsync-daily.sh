5ab45418e724dc1fc4a721d3f37a92015c46611e
Ciarán Mooney <general.mooney@googlemail.com>
2018-10-10 12:00:06 +0100

    Added --times parameter
    rsync will now check time modified.
    
    Also added comments about command arguments.

--- rsync-daily.sh
+++ rsync-daily.sh
@@ -1,1 +1,7 @@
-rsync -r --exclude-from exclude-list.lst /home /mnt/backup
+rsync -rvz --exclude-from exclude-list.lst --times /home /mnt/backup
+
+# -r - recursive
+# -v - verbose
+# -z - compress files during transfer
+# --times - compare times and copy if file modified
+#
