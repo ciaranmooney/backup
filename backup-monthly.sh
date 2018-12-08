e036612899a3f98434d85d70618f1879f9a94a87
Ciarán Mooney <general.mooney@googlemail.com>
2018-12-08 09:31:21 +0000

    Added description to server script
    Removed exclude list, on server the entire directy is backed up
    to offsite location.

--- backup-monthly.sh
+++ backup-monthly.sh
@@ -1,3 +1,6 @@
+# Backup script for server
+# Ciarân Mooney
+# 2018
+
 duplicity remove-all-inc-of-but-n-full 30 --force file:///mnt/backup
-duplicity --exclude-filelist exclude-list.lst /home file:///mnt/backup
 duplicity cleanup --force file:///mnt/backup
