6006102133d07f20481db5407ffa12f53b204bd1
Ciarán Mooney <general.mooney@googlemail.com>
2018-12-08 09:36:05 +0000

    Added pseudo-script for later.

--- backup-monthly.sh
+++ backup-monthly.sh
@@ -4,3 +4,5 @@
 
 duplicity remove-all-inc-of-but-n-full 30 --force file:///mnt/backup
 duplicity cleanup --force file:///mnt/backup
+# Check for "Complete" flag, ie a complete backup has been done.
+# Delete contents of source directory to allow new complete backup
