2e91c433a876a9f377d0a689b593c6ffff72a81a
Ciarán Mooney <general.mooney@googlemail.com>
2018-12-08 09:30:16 +0000

    Added psudo-script for later.

--- rsync-daily.sh
+++ rsync-daily.sh
@@ -1,10 +1,14 @@
+# Log start time
 rsync -rvz --exclude-from exclude-list.lst --times \
     --max-size=1M /home /mnt/backup
+# Log end time
+# Log start time
 rsync -rvz --exclude-from exclude-list.lst --times \
     --min-size=1M /home /mnt/backup
+# Log end time
 
 # -r - recursive
 # -v - verbose
 # -z - compress files during transfer
 # --times - compare times and copy if file modified
-#
+
