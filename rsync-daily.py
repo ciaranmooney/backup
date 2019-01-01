7680016da3bc95f77193a6024c20ec2aed23ba04
Ciarán Mooney <general.mooney@googlemail.com>
2019-01-01 10:20:16 +0000

    Created .py script
    Going to move over to python rather than bash.
    
    Used the original bash script as "template".
    
    Not yet valid python

---
+++ rsync-daily.py
@@ -0,0 +1,25 @@
+#! /usr/bin/env python
+
+# Client backup script
+# Ciarân Mooney
+# 2018
+
+# Check destination folder exists (ie wi-fi working, nfs working) 
+#
+# Log start time
+# Backup small files first
+rsync -rvz --exclude-from exclude-list.lst --times \
+    --max-size=1M /home /mnt/backup
+# Log end time
+# Log start time
+# Backup larger files second
+rsync -rvz --exclude-from exclude-list.lst --times \
+    --min-size=1M /home /mnt/backup
+# Log end time
+# Set complete flag
+
+# -r - recursive
+# -v - verbose
+# -z - compress files during transfer
+# --times - compare times and copy if file modified
+
