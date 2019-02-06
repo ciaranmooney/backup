bf123587d21b12bbbfd18de9518a7c400b0cb439
Ciarán Mooney <general.mooney@googlemail.com>
2019-02-06 20:17:58 +0000

    Removed python file and .sh

--- backup-monthly.sh
+++
@@ -1,8 +0,0 @@
-# Backup script for server
-# Ciarân Mooney
-# 2018
-
-duplicity remove-all-inc-of-but-n-full 30 --force file:///media/piDrive/UbuntuBackup
-duplicity cleanup --force file:///media/piDrive/UbuntuBackup
-# Check for "Complete" flag, ie a complete backup has been done.
-# Delete contents of source directory to allow new complete backup
